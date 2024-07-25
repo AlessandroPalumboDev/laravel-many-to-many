b5699b6cd70ae84126afad8962fd52272e0dbb70
Alessandro Palumbo <163033493+AlessandroPalumboDev@users.noreply.github.com>
2024-07-25 10:17:34 +0200

    feat: Aggiustato stile, riorganizzate card, spostati controller

--- resources/views/guest/projects/index.blade.php
+++ resources/views/guest/projects/index.blade.php
@@ -5,27 +5,37 @@
     <div class="container mt-3 text-light">
 
         <div class="row justify-content-center align-items-center">
+
             @foreach ($projects as $project)
-                <div class=" col-4 my-3">
-                    <div class="guest-card card bg-dark">
-                        <div class="card-header d-flex justify-content-between align-items-center bg-dark text-light">
-                            <h5>
+
+                <div class=" col-4 my-3 ">
+
+                    <div class="guest-card card bg-dark text-light">
+
+                        <div class="card-header py-3">
+                            <h4>
                                 {{$project->name}}
-                            </h5>
+                            </h4>
                         </div>
+
                         <div class="card-body">
-                            <p class="text-light">
+                            <p>
                                 {{$project->description}}
                             </p>
+                        </div>
+
+                        <div class="card-footer">
                             <a
                                 href="{{ route('projects.show', $project) }}"
                                 class="btn btn-outline-secondary btn-sm">
                                 <i class="fas fa-binoculars"></i>
                             </a>
                         </div>
-                        </div>
-                    </ul>
+
+                    </div>
+
                 </div>
+
             @endforeach
             
         </div>
--- resources/views/guest/projects/show.blade.php
+++ resources/views/guest/projects/show.blade.php
@@ -6,9 +6,11 @@
         <div class="row">
             <div class="card my-5 bg-dark text-light">
 
-                <h2 class="text-center my-3">
-                    {{ $project->name }}
-                </h2>
+                <div class="card-header">
+                    <h2 class="text-center my-3">
+                        {{ $project->name }}
+                    </h2>
+                </div>
 
                 <div class="card-body">
                     <h5>
@@ -18,6 +20,7 @@
                     <h5>
                         Tecnologie utilizzate: 
                     </h5>
+
                     <ul>
                         @foreach ($project->technology as $technology)
                             <li >
@@ -33,15 +36,15 @@
                         {{$project->description}}
                     </p>
 
-                    <div class="d-flex justify-content-between align-items-center mt-5">
-                        <a href="{{ route("projects") }}" class="btn btn-outline-secondary">
-                            <i class="fas fa-arrow-left"></i>
-                            Torna alla lista Progetti
-                        </a>
+   
+                </div>
+                <div class="card-footer py-3">
+
+                    <a href="{{ route("projects") }}" class="btn btn-outline-secondary">
+                        <i class="fas fa-arrow-left"></i>
+                        Torna alla lista Progetti
+                    </a>
     
-                        </div>
-                    </div>
-                        
                 </div>
             </div>
 
